5c64c9209fa9a8d004b9cab8d23c8b06af66b341
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2023-03-20 22:50:36 +0700

    class adapter

---
+++ ClassAdapter/Program.cs
@@ -0,0 +1,30 @@
+﻿public interface ExistingClass
+{
+    void Request();
+}
+
+public class Adaptee
+{
+    public void SpecificRequest()
+    {
+        Console.WriteLine("Specific request from Adaptee");
+    }
+}
+
+public class ClassAdapter : Adaptee, ExistingClass
+{
+    public void Request()
+    {
+        base.SpecificRequest();
+    }
+}
+
+
+public class Client
+{
+    static void Main()
+    {
+        ExistingClass target = new ClassAdapter();
+        target.Request();
+    }
+}
