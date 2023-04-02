33818b0025db63f1816e43f0fb12089ef27db275
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2023-04-02 09:05:50 +0700

    Composite & Prototype

---
+++ Composite/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+// Component interface
+public interface IComponent
+{
+    void Operation();
+}
+
+// Leaf class
+public class Leaf : IComponent
+{
+    public void Operation()
+    {
+        Console.WriteLine("Leaf operation.");
+    }
+}
+
+// Composite class
+public class Composite : IComponent
+{
+    private List<IComponent> _children = new List<IComponent>();
+
+    public void Add(IComponent component)
+    {
+        _children.Add(component);
+    }
+
+    public void Remove(IComponent component)
+    {
+        _children.Remove(component);
+    }
+
+    public void Operation()
+    {
+        Console.WriteLine("Composite operation.");
+        foreach (IComponent component in _children)
+        {
+            component.Operation();
+        }
+    }
+}
+
+// Client code
+class Client
+{
+    static void Main(string[] args)
+    {
+        // Create leaf components
+        Leaf leaf1 = new Leaf();
+        Leaf leaf2 = new Leaf();
+        Leaf leaf3 = new Leaf();
+
+        // Create composite components
+        Composite composite1 = new Composite();
+        Composite composite2 = new Composite();
+
+        // Add leaf components to composite1
+        composite1.Add(leaf1);
+        composite1.Add(leaf2);
+
+        // Add composite1 and leaf3 to composite2
+        composite2.Add(composite1);
+        composite2.Add(leaf3);
+
+        // Call Operation() on composite2
+        composite2.Operation();
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+
+public class Person : ICloneable
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Address Address { get; set; }
+
+    // Sao chép đối tượng
+    public object Clone()
+    {
+        return new Person
+        {
+            Name = this.Name,
+            Age = this.Age,
+            Address = this.Address.Clone() as Address
+        };
+    }
+}
+
+
+// Đối tượng phụ trợ
+public class Address : ICloneable
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+
+    // Sao chép đối tượng
+    public object Clone()
+    {
+        return new Address
+        {
+            Street = this.Street,
+            City = this.City
+        };
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var person1 = new Person
+        {
+            Name = "John",
+            Age = 30,
+            Address = new Address
+            {
+                Street = "123 Main St",
+                City = "New York"
+            }
+        };
+
+        // Sao chép đối tượng person1 để tạo person2
+        var person2 = person1.Clone() as Person;
+
+        // Thay đổi giá trị của person2
+        person2.Name = "Jane";
+        person2.Age = 25;
+        person2.Address.Street = "456 Oak St";
+
+        // In thông tin của person1 và person2
+        Console.WriteLine($"Person 1: {person1.Name}, {person1.Age}, {person1.Address.Street}, {person1.Address.City}");
+        Console.WriteLine($"Person 2: {person2.Name}, {person2.Age}, {person2.Address.Street}, {person2.Address.City}");
+    }
+}
