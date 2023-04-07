8d16d8417574d98ec6bc156d747955dfd47bfa39
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2023-04-07 08:20:23 +0700

    Decorator

---
+++ Bridge/Program.cs
@@ -0,0 +1,82 @@
+﻿// Implementor interface
+public interface IEffect
+{
+    void ApplyEffect();
+}
+
+// Concrete Implementor classes
+public class DamageEffect : IEffect
+{
+    public void ApplyEffect()
+    {
+        Console.WriteLine("Deals damage to the enemy.");
+    }
+}
+
+public class HealEffect : IEffect
+{
+    public void ApplyEffect()
+    {
+        Console.WriteLine("Heals an ally.");
+    }
+}
+
+// Abstraction class
+public abstract class Skill
+{
+    protected IEffect _effect;
+
+    public Skill(IEffect effect)
+    {
+        _effect = effect;
+    }
+
+    public abstract void Cast();
+}
+
+// Refined Abstraction classes
+public class OffensiveSkill : Skill
+{
+    public OffensiveSkill(IEffect effect) : base(effect)
+    {
+    }
+
+    public override void Cast()
+    {
+        Console.WriteLine("Cast an offensive skill.");
+        _effect.ApplyEffect();
+    }
+}
+
+public class SupportSkill : Skill
+{
+    public SupportSkill(IEffect effect) : base(effect)
+    {
+    }
+
+    public override void Cast()
+    {
+        Console.WriteLine("Cast a supportive skill.");
+        _effect.ApplyEffect();
+    }
+}
+
+// Client code
+public class Champion
+{
+    public OffensiveSkill OffensiveSkill { get; set; }
+    public SupportSkill SupportSkill { get; set; }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Champion champion = new Champion();
+        champion.OffensiveSkill = new OffensiveSkill(new DamageEffect());
+        champion.SupportSkill = new SupportSkill(new HealEffect());
+
+        champion.OffensiveSkill.Cast();
+        champion.SupportSkill.Cast();
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Numerics;
+
+// Interface cho tướng
+interface IChampion
+{
+    string Name { get; }
+    int AttackDamage { get; }
+    string Cast();
+}
+
+// Lớp cơ sở cho tướng
+class Yasuo : IChampion
+{
+    public Yasuo(int attackDamage)
+    {
+        Name = "Yasuo";
+        AttackDamage = attackDamage;
+    }
+
+    public string Name { get; set; }
+    public int AttackDamage { get; set; }
+    public string Cast()
+    { 
+        return "Yasuo cast: ";
+    }
+}
+
+// Lớp Decorator cho SummonerSpell
+abstract class SummonerSpellDecorator : IChampion
+{
+    protected IChampion champion;
+    public SummonerSpellDecorator(IChampion champion)
+    {
+        this.champion = champion;
+    }
+
+    public virtual string Name { get; set; }
+    public virtual int AttackDamage { get; set; }
+
+    public virtual string Cast()
+    {
+       return champion.Cast();
+    }
+}
+
+// Lớp Decorator cho Teleport SummonerSpell
+class TeleportSummonerSpellDecorator : SummonerSpellDecorator
+{
+    public TeleportSummonerSpellDecorator(IChampion champion) : base(champion)
+    {
+    }
+
+    public override string Name
+    {
+        get
+        {
+            return champion.Name + " with Teleport Summoner Spell";
+        }
+        set { }
+    }
+
+    public override int AttackDamage
+    {
+        get
+        {
+            return champion.AttackDamage;
+        }
+        set { }
+    }
+
+    // Thêm tính năng bổ sung cho SummonerSpell
+    public override string Cast()
+    {
+        return base.Cast() + " teleport,";
+    }
+
+}
+
+class FlashSummonerSpellDecorator : SummonerSpellDecorator
+{
+    public FlashSummonerSpellDecorator(IChampion champion) : base(champion)
+    {
+    }
+
+    public override string Name
+    {
+        get
+        {
+            return champion.Name + " with Flash Summoner Spell";
+        }
+        set { }
+    }
+
+    public override int AttackDamage
+    {
+        get
+        {
+            return champion.AttackDamage;
+        }
+        set { }
+    }
+
+    // Thêm tính năng bổ sung cho SummonerSpell
+    public override string Cast()
+    {
+        return base.Cast() + " flash,";
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Tạo ra tướng Ezreal
+        IChampion yasuo = new Yasuo(50);
+        Console.WriteLine(yasuo.Cast());
+
+
+        // Thêm tính năng Teleport Summoner Spell
+        IChampion yasuoWithTeleport = new TeleportSummonerSpellDecorator(yasuo);
+        Console.WriteLine(yasuoWithTeleport.Cast());
+
+        //Thêm tính năng Flash
+        IChampion yasuoWithTeleportFlash = new FlashSummonerSpellDecorator(yasuoWithTeleport);
+        Console.WriteLine(yasuoWithTeleportFlash.Cast());
+    }
+}
