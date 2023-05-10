6ca656d27543f09fb093171c7bb7df1d55fcf2eb
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2023-05-10 13:14:21 +0700

    proxy + state

---
+++ Proxy/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+// Subject interface
+public interface ICarService
+{
+    void GetCarDetails();
+}
+
+// Real Subject
+public class CarService : ICarService
+{
+    public void GetCarDetails()
+    {
+        Console.WriteLine("Fetching car details...");
+    }
+}
+
+// Proxy
+public class CarServiceProxy : ICarService
+{
+    private readonly string _username;
+    private readonly string _password;
+    private CarService _carService;
+
+    public CarServiceProxy(string username, string password)
+    {
+        _username = username;
+        _password = password;
+    }
+
+    public void GetCarDetails()
+    {
+        if (Authenticate())
+        {
+            if (_carService == null)
+            {
+                _carService = new CarService();
+            }
+
+            _carService.GetCarDetails();
+        }
+        else
+        {
+            Console.WriteLine("Authentication failed. Access denied.");
+        }
+    }
+
+    private bool Authenticate()
+    {
+        // Perform authentication logic here, e.g. check credentials against database
+        return (_username == "admin" && _password == "password");
+    }
+}
+
+// Client code
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Proxy is used to get car details
+        ICarService carServiceProxy = new CarServiceProxy("admin", "password");
+        carServiceProxy.GetCarDetails();
+
+        // Proxy is used again to get car details, but authentication will fail this time
+        carServiceProxy = new CarServiceProxy("invalid", "password");
+        carServiceProxy.GetCarDetails();
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,98 @@
+﻿public interface IOrderState
+{
+    void ProcessOrder(Order order);
+    string Status { get; }
+}
+
+public class NewOrderState : IOrderState
+{
+    public void ProcessOrder(Order order)
+    {
+        // xử lý đơn hàng mới ở đây
+        order.State = new ShippedOrderState();
+    }
+
+    public string Status
+    {
+        get { return "New Order"; }
+    }
+}
+
+public class ShippedOrderState : IOrderState
+{
+    public void ProcessOrder(Order order)
+    {
+        // xử lý đơn hàng đã được gửi đi ở đây
+        order.State = new DeliveredOrderState();
+    }
+
+    public string Status
+    {
+        get { return "Shipped"; }
+    }
+}
+
+public class DeliveredOrderState : IOrderState
+{
+    public void ProcessOrder(Order order)
+    {
+        // xử lý đơn hàng đã được giao đến ở đây
+        order.State = new ClosedOrderState();
+    }
+
+    public string Status
+    {
+        get { return "Delivered"; }
+    }
+}
+
+public class ClosedOrderState : IOrderState
+{
+    public void ProcessOrder(Order order)
+    {
+        // không làm gì cả, vì đơn hàng đã đóng
+    }
+
+    public string Status
+    {
+        get { return "Closed"; }
+    }
+}
+
+public class Order
+{
+    public IOrderState State { get; set; }
+
+    public Order()
+    {
+        State = new NewOrderState();
+    }
+
+    public void Process()
+    {
+        State.ProcessOrder(this);
+    }
+
+    public string Status
+    {
+        get { return State.Status; }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Order order = new Order();
+        Console.WriteLine(order.Status); // "New Order"
+
+        order.Process();
+        Console.WriteLine(order.Status); // "Shipped"
+
+        order.Process();
+        Console.WriteLine(order.Status); // "Delivered"
+
+        order.Process();
+        Console.WriteLine(order.Status); // "Closed"
+
+    }
+}
